cba1bc25b943d93dec82713bf303af944983f5c1
dallas <themaindennis@gmail.com>
2026-06-16 09:37:55 +0200

    Finished up printer setup

--- modules/M2070.nix
+++ modules/M2070.nix
@@ -6,4 +6,16 @@
   services.printing.drivers = [ pkgs.samsung-unified-linux-driver ];
   hardware.sane.enable = true;
   hardware.sane.extraBackends = [ pkgs.samsung-unified-linux-driver ];
+
+  hardware.printers.ensurePrinters = [
+  {
+    name = "M2070";
+    location = "Right Here";
+    deviceUri = "usb://Samsung/M2070%20Series?serial=ZF44BJCH6000LKD&interface=1";
+    model = "samsung/M267x.ppd";
+    ppdOptions = {
+      PageSize = "A4";
+    };
+  }
+];
 }
